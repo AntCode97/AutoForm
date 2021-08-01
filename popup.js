d2a9a95a54d222e28cfacbf25c214ba39ea64e13
AntCode97 <50140266+AntCode97@users.noreply.github.com>
2021-08-02 01:27:53 +0900

    기본 기능 완성

--- popup.js
+++ popup.js
@@ -1,14 +1,34 @@
-// Initialize button with user's preferred color
+﻿// Initialize button with user's preferred color
 let start = document.getElementById("start");
 // let formText = document.getElementById("formText").value;
 
-chrome.storage.sync.get("color", ({ color }) => {
-    start.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//     start.style.backgroundColor = color;
+// });
+
+//TODO: 종료버튼도 만들어야함
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log("hi");
+
+//     chrome.tabs.executeScript(null, { code: "document.body.style.backgroundColor='red'" });
+//     // chrome.tabs.executeScript({
+//     //     code: 'document.querySelector("div#input.yt-live-chat-text-input-field-renderer")'
+//     // }, function (result) {
+//     //     console.log("HI");
+//     //     console.log(result);
+//     // });
+
+// });
+
+
+
+//div.querySelector(".tit-box > .fl > table > tbody > tr > td > .b").innerText;
+
 
 // start.addEventListener("click", starting);
 
-// When the button is clicked, inject setPageBackgroundColor into current page
+//When the button is clicked, inject setPageBackgroundColor into current page
 start.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     console.log("hi");
@@ -24,21 +44,19 @@ function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
         document.body.style.backgroundColor = color;
     });
-} function autoNaming() {
-    var chat = document.querySelector("div#input.yt-live-chat-text-input-field-renderer");
-    //console.log(formText.value);
-
-    if (chat.innerText == "") {
-        chat.innerText = "광주";
-    }
 }
+
+
 function starting() {
     window.setInterval(() => {
-        var chat = document.querySelector("div#input.yt-live-chat-text-input-field-renderer");
-        console.log(chat);
-        if (chat.innerText == "") {
-            chat.innerText = "광주";
+
+        console.log("testset");
+        var chatFrame = document.querySelector("#chatframe").contentDocument;
+        var chatInput = chatFrame.querySelector("div#input.yt-live-chat-text-input-field-renderer");
+        if (chatInput.innerText == "") {
+            chatInput.innerText = "[광주_4반_이윤준]";
         }
     }, 1000);
 
 }
+
