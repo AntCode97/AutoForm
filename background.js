8089934f9fad98f9fb3de20a12f9bc1833b6c574
AntCode97 <50140266+AntCode97@users.noreply.github.com>
2021-08-02 23:52:26 +0900

    안쓰는 파일 제거, 로고 이미지 변경, 중지 버튼 추가

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-let color = '#3aa757';
+// let formText = '채팅 형식을 입력해주세요';
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.storage.sync.set({ formText });
+//     console.log('Default background color set to %cgreen', `color: ${formText}`);
+// });
--- popup.js
+++ popup.js
@@ -1,75 +1,41 @@
-﻿// Initialize button with user's preferred color
-let start = document.getElementById("start");
+﻿let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
 
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//     start.style.backgroundColor = color;
-// });
-
-//TODO: 종료버튼도 만들어야함
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     console.log("hi");
-
-//     chrome.tabs.executeScript(null, { code: "document.body.style.backgroundColor='red'" });
-//     // chrome.tabs.executeScript({
-//     //     code: 'document.querySelector("div#input.yt-live-chat-text-input-field-renderer")'
-//     // }, function (result) {
-//     //     console.log("HI");
-//     //     console.log(result);
-//     // });
-
-// });
-
-
-
-//div.querySelector(".tit-box > .fl > table > tbody > tr > td > .b").innerText;
-
-
-// start.addEventListener("click", starting);
-
-//When the button is clicked, inject setPageBackgroundColor into current page
-start.addEventListener("click", async () => {
+startBtn.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     console.log("hi");
     let inputText = document.getElementById("formText").value;
     console.log(inputText);
 
-    // chrome.storage.sync.set({ "formText": inputText }, function () {
-    //     console.log("value is set to " + inputText);
-    // });
-    // let tabId = tab.id;
-    // chrome.tabs.executeScript(
-    //     tab.id, {
-    //     code: `console.log("hi");`
-    // }, function () {
-    //     chrome.tabs.executeScript(tab.id, { file: 'content.js' })
-    // });
+    chrome.storage.sync.set({ "formText": inputText }, function () {
+        console.log("value is set to " + inputText);
+    });
 
-    // storage 기능을 이용하여 변수를 전달해야 한다고 한다
-    // https://developer.chrome.com/docs/extensions/reference/scripting/
-    //https://developer.chrome.com/docs/extensions/reference/storage/
     chrome.scripting.executeScript({
         target: {
             tabId: tab.id
         },
-        function: starting,
+        function: start,
     });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
+stopBtn.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    console.log("hi");
+
+    chrome.scripting.executeScript({
+        target: {
+            tabId: tab.id
+        },
+        function: stop,
     });
-}
+});
 
 
-function starting() {
-    window.setInterval(() => {
+function start() {
+    let makeForm = setInterval(() => {
         chrome.storage.sync.get(['formText'], function (result) {
-            console.log('Value currently is ' + result.formText)
+            //console.log('Value currently is ' + result.formText)
             //    console.log(formText);
             var chatFrame = document.querySelector("#chatframe").contentDocument;
             var chatInput = chatFrame.querySelector("div#input.yt-live-chat-text-input-field-renderer");
@@ -83,5 +49,17 @@ function starting() {
 
     }, 1000);
 
+    chrome.storage.sync.set({ "makeForm": makeForm }, function () {
+        //console.log("value is set to " + makeForm);
+    });
 }
 
+
+function stop() {
+
+    chrome.storage.sync.get(['makeForm'], function (result) {
+        // console.log('Value currently is ' + result.makeForm)
+        clearInterval(result.makeForm);
+    });
+
+}
