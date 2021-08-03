b957da0b353a3a7bc20207ce36733a7c335b7b83
AntCode97 <50140266+AntCode97@users.noreply.github.com>
2021-08-04 01:42:52 +0900

    에약 기능 추가 중

--- popup.js
+++ popup.js
@@ -21,7 +21,6 @@ startBtn.addEventListener("click", async () => {
 
 stopBtn.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    console.log("hi");
 
     chrome.scripting.executeScript({
         target: {
@@ -37,12 +36,23 @@ function start() {
         chrome.storage.sync.get(['formText'], function (result) {
             //console.log('Value currently is ' + result.formText)
             //    console.log(formText);
-            var chatFrame = document.querySelector("#chatframe").contentDocument;
-            var chatInput = chatFrame.querySelector("div#input.yt-live-chat-text-input-field-renderer");
-            if (chatInput.innerText == "") {
-                chatInput.innerText = result.formText;
-                //            chatInput.innerText = "[광주_4반_이윤준]";
+
+            var chat = document.querySelector("div#input.style-scope.yt-live-chat-text-input-field-renderer");
+            if (chat != null) {
+                if (chat.innerText == "") {
+                    chat.innerText = result.formText;
+                    //            chatInput.innerText = "[광주_4반_이윤준]";
+                }
+            } else {
+                var chatFrame = document.querySelector("#chatframe").contentDocument;
+                var chatInput = chatFrame.querySelector("div#input.yt-live-chat-text-input-field-renderer");
+                if (chatInput.innerText == "") {
+                    chatInput.innerText = result.formText;
+                    //            chatInput.innerText = "[광주_4반_이윤준]";
+                }
             }
+
+
 
         });
 
@@ -54,12 +64,46 @@ function start() {
     });
 }
 
+function start2() {
+    let chatFrame = document.querySelector("#chatframe").contentDocument;
+    let chatInput = chatFrame.querySelector("div#input.yt-live-chat-text-input-field-renderer");
+
+
+    var makeForm = function () {
+        chrome.storage.sync.get(['formText'], function (result) {
+            //console.log('Value currently is ' + result.formText)
+            //    console.log(formText);
+            if (chatInput.innerText == "") {
+                chatInput.innerText = result.formText;
+            }
+        });
+
+    }
+
+    chatInput.addEventListener("click", makeForm)
+
+
+    chrome.storage.sync.set({ "makeForm": makeForm }, function () {
+        //console.log("value is set to " + makeForm);
+    });
+}
 
 function stop() {
 
     chrome.storage.sync.get(['makeForm'], function (result) {
         // console.log('Value currently is ' + result.makeForm)
         clearInterval(result.makeForm);
+    });
+
+}
+
+function stop2() {
+
+    chrome.storage.sync.get(['makeForm'], function (result) {
+        let chatFrame = document.querySelector("#chatframe").contentDocument;
+        let chatInput = chatFrame.querySelector("div#input.yt-live-chat-text-input-field-renderer");
+
+        chatInput.removeEventListener('click', result.makeForm);
     });
 
 }
